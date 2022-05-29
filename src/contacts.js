1fe6b1dde3d5b9a5ec59d5bf64f2aad0ece875b9
Kolyganov Ivan <adamjensen2302@gmail.com>
2022-05-29 23:00:55 +0500

    Оптимизация производительности

--- src/contacts.js
+++ src/contacts.js
@@ -1,26 +1,34 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
-function addContacts() {
+function addContacts(displayNumber, start) {
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
+
+  for (let i = 0; i < displayNumber; i++) {
     const child = document.createElement("div");
-    child.textContent = i;
+    child.textContent = start + i;
     child.classList.add("contact");
     fragment.appendChild(child);
   }
+
   contacts.appendChild(fragment);
 }
 
 contacts.addEventListener("scroll", (e) => {
   const items = Array.from(contacts.getElementsByClassName("contact"));
+
   const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
+  let topItemIndex = itemOffsets.findIndex(
+      (offset) => contacts.scrollTop - offset <= -18
   );
+  //Условие подгрузки новых контактов
+  if(topItemIndex + 20 > itemOffsets.length){
+    addContacts(20, itemOffsets.length);
+  }
+
   if (topItemIndex !== -1) {
     stickyHeader.textContent = items[topItemIndex].textContent;
   }
 });
 
-addContacts();
+addContacts(40, 0);
