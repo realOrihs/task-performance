0d6cf3ad965156e5d25a25a2b4d8c4d81add0e9f
Kolyganov Ivan <adamjensen2302@gmail.com>
2022-06-08 22:42:08 +0500

    Fixed task-performance

--- src/contacts.js
+++ src/contacts.js
@@ -1,34 +1,22 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
-function addContacts(displayNumber, start) {
+function addContacts() {
   const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < displayNumber; i++) {
-    const child = document.createElement("div");
-    child.textContent = start + i;
-    child.classList.add("contact");
-    fragment.appendChild(child);
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < 50000; i++) {
+      const child = document.createElement("div");
+      child.textContent = i;
+      child.classList.add("contact");
+      fragment.appendChild(child);
+    }
+    contacts.appendChild(fragment);
   }
-
-  contacts.appendChild(fragment);
 }
-
 contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-
-  const itemOffsets = items.map((item) => item.offsetTop);
-  let topItemIndex = itemOffsets.findIndex(
-      (offset) => contacts.scrollTop - offset <= -18
-  );
-  //Условие подгрузки новых контактов
-  if(topItemIndex + 20 > itemOffsets.length){
-    addContacts(20, itemOffsets.length);
-  }
-
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
+  stickyHeader.textContent = Math.floor(contacts.scrollTop / 18);
 });
 
-addContacts(40, 0);
+addContacts();
