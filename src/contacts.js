a1008d321486fdeb2ba5d7f2c7e1edabbbca9af3
Kolyganov Ivan <adamjensen2302@gmail.com>
2022-06-08 23:03:59 +0500

    Fix 2

--- src/contacts.js
+++ src/contacts.js
@@ -3,18 +3,15 @@ const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
 function addContacts() {
   const fragment = document.createDocumentFragment();
-
-  for (let i = 0; i < displayNumber; i++) {
-    const fragment = document.createDocumentFragment();
-    for (let i = 0; i < 50000; i++) {
-      const child = document.createElement("div");
-      child.textContent = i;
-      child.classList.add("contact");
-      fragment.appendChild(child);
-    }
-    contacts.appendChild(fragment);
+  for (let i = 0; i < 50000; i++) {
+    const child = document.createElement("div");
+    child.textContent = i;
+    child.classList.add("contact");
+    fragment.appendChild(child);
   }
+  contacts.appendChild(fragment);
 }
+
 contacts.addEventListener("scroll", (e) => {
   stickyHeader.textContent = Math.floor(contacts.scrollTop / 18);
 });
